ff7b4fd2d9f17f3e25bacd3327a9d25ce2658d2b
romainD <romain.deschamps61@sfr.fr>
2026-02-07 23:04:48 +0100

    Update TLD 2.51

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ImprovedRestForAll")]
 [assembly: AssemblyCopyright("Romain")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: MelonInfo(typeof(ImprovedRestForAll.Main), "ImprovedRestForAll", "1.0.0", "Romain")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: MelonInfo(typeof(ImprovedRestForAll.Main), "ImprovedRestForAll", "1.0.1", "Romain")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
